6a15575f3a56be6554749b1283bac66e3be73933
TristanT0 <tristan.toruke@ut.ee>
2023-11-05 22:24:17 +0200

    Dynamic Posting

--- script.js
+++ script.js
@@ -17,13 +17,35 @@ window.onload = function() {
                 const postDate = document.createElement('p');
                 postDate.classList.add('post-date');
                 postDate.textContent = post.create_time;
+                const postHeaderAuthor = document.createElement('p');
+                postHeaderAuthor.textContent = post.author;
+                postHeaderAuthor.classList.add('post-author')
                 postHeader.appendChild(postHeaderImage);
+                postHeader.appendChild(postHeaderAuthor);
                 postHeader.appendChild(postDate);
-
+                
+                
               
 
               
                 postDiv.appendChild(postHeader);
+                if(post.image != null) {
+                    const postImage = document.createElement('img');
+                    postImage.classList.add('post-image');
+                    postImage.src = post.image;
+                    postDiv.appendChild(postImage);
+                }
+                const postContent = document.createElement('p');
+                postContent.classList.add('post-content');
+                postContent.textContent = post.text;
+                postDiv.appendChild(postContent);
+                const postFooter = document.createElement('button');
+                postFooter.type = "submit";
+                postFooter.textContent = "Like"
+                postFooter.classList.add('post-footer');
+                postDiv.appendChild(postFooter)
+
+                
                 
 
                 
@@ -38,3 +60,6 @@ window.onload = function() {
         })
         
 }
+
+
+
