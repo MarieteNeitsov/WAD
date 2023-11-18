8c573f3030c704acc13c1225cefeb0d15a0d67df
marieten <neitsov.mariete@ut.ee>
2023-11-18 17:08:36 +0200

    vue structure

---
+++ vue-app/src/components/Footer.vue
@@ -0,0 +1,22 @@
+<template>
+    <footer>
+        <div class = "footer">
+            <p> Copyright © 2023 postIt <br>
+            <a href="mailto:example@example.com">example@example.com</a>
+            </p>
+        </div>
+    </footer>
+</template>
+
+<style scoped>
+    .footer {
+    width: 100%;
+    text-align: center;
+    position:absolute;
+    height: 50px;
+    background-image: linear-gradient(to right, #ABD6F8, #86B2D3);
+    margin-top: 50px;
+    bottom: 0;
+    left:0;
+}
+</style>
---
+++ vue-app/src/components/Header.vue
@@ -0,0 +1,18 @@
+<template>
+    <header>
+
+    </header>
+</template>
+
+<style scoped>
+    header{
+    width: 100%;
+    height: 50px;
+    background-image: linear-gradient(to right, #ABD6F8, #86B2D3);
+    justify-content: space-between;
+    align-items: left;
+    display: flex;
+    padding: 10px;
+    }
+    
+</style>
