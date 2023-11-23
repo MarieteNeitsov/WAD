a984506867b538c3cef0c9aecac97a85b93b8e1f
marieten <neitsov.mariete@ut.ee>
2023-11-23 16:05:19 +0200

    fixed footer

--- src/components/FooterComponent.vue
+++ src/components/FooterComponent.vue
@@ -1,9 +1,16 @@
 <template>
     <footer>
-        <div class = "footer">
+        <div class = "footercontainer">
             <p> Copyright © 2023 postIt </p> <br>
             <a href="mailto:example@example.com">example@example.com</a>
+
             
         </div>
     </footer>
 </template>
+
+<style scoped>
+
+
+
+</style>
