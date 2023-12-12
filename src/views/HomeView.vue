f30f14056c2b293788a1126c7aa05a7fbd6b1116
marieten <neitsov.mariete@ut.ee>
2023-12-12 17:41:30 +0200

    posts

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -13,7 +13,7 @@
   import HeaderComponent from '@/components/HeaderComponent.vue'
   import HomeComponent from '@/components/HomeComponent.vue'
   import FooterComponent from '@/components/FooterComponent.vue'
-  import { mapState } from 'vuex';
+
   
   export default {
     name: 'HomeView',
